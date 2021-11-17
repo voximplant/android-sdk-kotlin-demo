dff5b992569e5fe79a246945030aec9c13ef153d
Oleg Yakovlev <oyakovlev@voximplant.com>
2021-11-17 18:51:42 +0300

    [videocall] Added reconnectingIfNeed to "call back" on CallFailed screen

--- videocall-deepar/src/main/java/com/voximplant/demos/kotlin/videocall_deepar/stories/call_failed/CallFailedViewModel.kt
+++ videocall-deepar/src/main/java/com/voximplant/demos/kotlin/videocall_deepar/stories/call_failed/CallFailedViewModel.kt
@@ -3,6 +3,8 @@ package com.voximplant.demos.kotlin.videocall_deepar.stories.call_failed
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.voximplant.demos.kotlin.utils.*
+import com.voximplant.demos.kotlin.utils.Shared.authService
+import com.voximplant.demos.kotlin.utils.Shared.voximplantCallManager
 
 class CallFailedViewModel : BaseViewModel() {
     val displayName = MutableLiveData<String>()
@@ -12,8 +14,8 @@ class CallFailedViewModel : BaseViewModel() {
     override fun onCreate() {
         super.onCreate()
         displayName.postValue(
-            Shared.voximplantCallManager.latestCallerDisplayName
-                ?: Shared.voximplantCallManager.latestCallerDisplayName.orEmpty()
+            voximplantCallManager.latestCallerDisplayName
+                ?: voximplantCallManager.latestCallerDisplayName.orEmpty()
         )
     }
 
@@ -22,12 +24,25 @@ class CallFailedViewModel : BaseViewModel() {
     }
 
     fun callBack() {
-        try {
-            Shared.voximplantCallManager.createCall(Shared.voximplantCallManager.latestCallerUsername.orEmpty())
-            moveToCall.postValue(Unit)
-        } catch (e: CallManagerException) {
-            Log.e(APP_TAG, e.message.toString())
-            moveToMainActivity.postValue(Unit)
+        showProgress.postValue(R.string.reconnecting)
+        authService.reconnectIfNeeded { error ->
+            hideProgress.postValue(Unit)
+            error?.let {
+                if (error == AuthError.NetworkIssues) {
+                    postError(R.string.error_failed_to_reconnect_and_check_connectivity)
+                } else {
+                    moveToMainActivity.postValue(Unit)
+                }
+            } ?: run {
+                try {
+                    voximplantCallManager.createCall(voximplantCallManager.latestCallerUsername.orEmpty())
+                    moveToCall.postValue(Unit)
+                } catch (e: CallManagerException) {
+                    Log.e(APP_TAG, e.message.toString())
+                    postError(e.message.toString())
+                }
+            }
         }
     }
+
 }
