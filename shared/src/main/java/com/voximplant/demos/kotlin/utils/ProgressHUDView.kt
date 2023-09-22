86d981de646e5f0a578d98f2cccb63f2619aa7b5
Oleg Yakovlev <oyakovlev@voximplant.com>
2023-09-22 19:01:45 +0300

    (shared) Deprecated Kotlin synthetics replaced with Jetpack view binding

--- shared/src/main/java/com/voximplant/demos/kotlin/utils/ProgressHUDView.kt
+++ shared/src/main/java/com/voximplant/demos/kotlin/utils/ProgressHUDView.kt
@@ -2,18 +2,23 @@ package com.voximplant.demos.kotlin.utils
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.LinearLayout
-import kotlinx.android.synthetic.main.progress_hud_layout.view.*
+import com.voximplant.demos.kotlin.utils.databinding.ProgressHudLayoutBinding
 
 class ProgressHUDView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
 ) : LinearLayout(context, attrs, defStyleAttr) {
+    private var binding: ProgressHudLayoutBinding
 
     init {
         inflate(R.layout.progress_hud_layout, true)
+        binding = ProgressHudLayoutBinding.inflate(LayoutInflater.from(context), this, true)
     }
 
     fun setText(text: String) {
-        loading_text_label.text = text
+        binding.loadingTextLabel.text = text
     }
 }
